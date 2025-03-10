00ebbc59537a7ae321af612cd6b7ced9d4124bb8
demon20091 <dmitry199544@gmail.com>
2025-03-10 18:35:36 +0300

    edit widget.py

--- src/widget.py
+++ src/widget.py
@@ -7,19 +7,13 @@ from src.masks import get_masks_card_number, get_mask_account
 def mask_account_card(type_and_number: Union[str]) -> Union[str]:
     '''функция, которая умеет обрабатывать информацию как о картах, так и о счетах.'''
 
-    text_result = ""
-    digit_result = ""
-    digit_count = 0
-    for el in type_and_number:
-        if el.isalpha():
-            text_result += el
-        elif el.isdigit():
-            digit_result += el
-            digit_count += 1
-    if digit_count > 16:
-        return f"{text_result} {get_mask_account(digit_result)}"
+    parts = type_and_number.split()
+    only_type = ' '.join(parts[:-1])
+    only_number = parts[-1]
+    if len(only_number) > 16: # ещё лучше - if only_type.lower().startswith('счет')
+        return f"{only_type} {get_mask_account(only_number)}"
     else:
-        return f"{text_result} {get_masks_card_number(digit_result)}"
+        return f"{only_type} {get_masks_card_number(only_number)}"
 
 
 def get_date(user_date: Union[str]) -> Union[str]:
@@ -32,6 +26,6 @@ def get_date(user_date: Union[str]) -> Union[str]:
     return new_date
 
 
-print(mask_account_card("VisaPlatinum 1234567891234567"))
+print(mask_account_card("Visa Platinum 1234567891234567"))
 print(mask_account_card("Счет 34565789789456132465"))
 print(get_date("2024-03-11T02:26:18.671407"))  # 11.03.2024
